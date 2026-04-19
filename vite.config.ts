27f905af5b21e3a76474dc16980d4832bca83e9e
MonaRose1 <maimoonanaumani2@gmail.com>
2026-04-19 23:33:25 +0500

    Fix GitHub Pages deployment and base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio2.0/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
